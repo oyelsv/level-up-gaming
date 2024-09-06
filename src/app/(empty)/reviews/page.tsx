e2c7c8fa58a9d1bb352953d74f05b4ea2cd9ef8e
oyelsv <oyelsv@gmail.com>
2024-09-06 23:50:49 +0200

    feat: add empty pages

---
+++ src/app/(empty)/reviews/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ReviewsPage() {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold">Reviews</h1>
+    </div>
+  );
+}
+
+export default ReviewsPage;
+
+ReviewsPage.displayName = 'ReviewsPage';
